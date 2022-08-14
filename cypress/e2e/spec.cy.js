b8f613b2d40f04443a4c7811a85d29e5dd00b9c5
Tony White <tonywhite@gmail.com>
2022-08-13 21:03:56 -0500

    copy: changed copy of h1

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('/')
-    cy.get('h1').should('contain.text', 'Welcome to').should('be.visible')
+    cy.get('h1').should('contain.text', 'Welcome tttto').should('be.visible')
   })
 })
